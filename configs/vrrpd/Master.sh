ea767ba21ccc6badce1bdf9c4196f9dc287a9d0d
Fredb <fredbcode@users.noreply.github.com>
2016-12-23 11:31:55 +0100

    Update Master.sh

--- configs/vrrpd/Master.sh
+++ configs/vrrpd/Master.sh
@@ -1,15 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 # When vrrpd become Master
-
+# 
 #-------------------------------
+# THIS IS A SAMPLE FILE WITH ONE IP ALIAS 
+# ADAPT TO YOUR OWN CONFIGURATION !
+
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
-#variables
 horodate=$(date +%d/%m/%Y_%R)
 ##--------------------------------##
 
 echo "1" > /proc/sys/net/ipv4/ip_forward
 
 echo "Montage int"
+# DON'T FORGET ! MUST BE REMOVED IN backup.sh
 ifconfig eth1:0 192.168.14.2 netmask 255.255.255.0 up
 
 echo "ROUTE"
