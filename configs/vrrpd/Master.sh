6cc099156b66068c4d5230473a7bcf0e7dc35170
Fredb <fredbcode@users.noreply.github.com>
2016-12-23 11:40:56 +0100

    Update Master.sh

--- configs/vrrpd/Master.sh
+++ configs/vrrpd/Master.sh
@@ -14,6 +14,9 @@ echo "1" > /proc/sys/net/ipv4/ip_forward
 echo "Montage int"
 # DON'T FORGET ! MUST BE REMOVED IN backup.sh
 ifconfig eth1:0 192.168.14.2 netmask 255.255.255.0 up
+# IF NEEDEED
+# REFRESH MAC
+# ping -c 2 -I 192.168.14.2 192.168.14.254 (GATEWAY)
 
 echo "ROUTE"
 /etc/init.d/route.sh
